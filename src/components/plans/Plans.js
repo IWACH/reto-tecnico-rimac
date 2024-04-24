f455e52048d273a769fc8b60b5a27955e0de8719
Iwach <irwinwalter70@gmail.com>
2024-04-24 11:38:01 -0500

    add Link for Back Button

--- src/components/plans/Plans.js
+++ src/components/plans/Plans.js
@@ -7,6 +7,7 @@ import { getProducts } from "@/lib/features/productSlice";
 import CardProduct from "./CardProduct";
 import { map, filter } from "lodash";
 import dayjs from "dayjs";
+import Link from "next/link";
 
 const Plans = () => {
   const dispatch = useAppDispatch();
@@ -62,71 +63,75 @@ const Plans = () => {
     },
   ];
 
+  const BackButton = () => (
+    <Link className="plans-back" href="/">
+      <i className="fa-solid fa-circle-chevron-left"></i>
+      <span>Volver</span>
+    </Link>
+  );
+
+  const MainContainer = ({ children }) => (
+    <div className="main">
+      <div className="plans-container grid">
+        <BackButton />
+        {children}
+      </div>
+    </div>
+  );
+
   if (userLoading || loading) {
     return (
-      <div className="main">
-        <div className="plans-container grid">
-          <div className="plans-back">
-            <i className="fa-solid fa-circle-chevron-left"></i>
-            <span>Volver</span>
-          </div>
-          <div className="box-plan-title">
-            <div className="plans-title">Cargando...</div>
-          </div>
+      <MainContainer>
+        <div className="box-plan-title">
+          <div className="plans-title">Cargando...</div>
         </div>
-      </div>
+      </MainContainer>
     );
   }
 
   return (
-    <div className="main">
-      <div className="plans-container grid">
-        <div className="plans-back">
-          <i className="fa-solid fa-circle-chevron-left"></i>
-          <span>Volver</span>
+    <MainContainer>
+      <div className="box-plan-title">
+        <div className="plans-title">{`${users?.name}, ¿Para quién deseas cotizar?`}</div>
+        <div className="plans-subtitle">
+          Selecciona la opción que se ajuste más a tus necesidades.
         </div>
-        <div className="box-plan-title">
-          <div className="plans-title">{`${users?.name}, ¿Para quién deseas cotizar?`}</div>
-          <div className="plans-subtitle">
-            Selecciona la opción que se ajuste más a tus necesidades.
-          </div>
-        </div>
-
-        <div className="plans-cards">
-          {planInfo.map((plan, index) => (
-            <CardPlanInfo
-              key={index}
-              title={plan.title}
-              icon={plan.icon}
-              description={plan.description}
-              onCheckboxChange={() => handleCheckboxChange(index)}
-              isSelected={selectedPlan === index}
-            />
-          ))}
-        </div>
-
-        {selectedPlan !== null && (
-          <div className="box-products">
-            {products &&
-              map(
-                filter(products, (product) => product.age >= userAge),
-                (product) => {
-                  const discount = selectedPlan === 1;
-                  return (
-                    <CardProduct
-                      description={product.description}
-                      name={product.name}
-                      discount={discount}
-                      price={product.price}
-                      icon="/products/icon-product-1.svg"
-                    />
-                  );
-                }
-              )}
-          </div>
-        )}
       </div>
-    </div>
+
+      <div className="plans-cards">
+        {planInfo.map((plan, index) => (
+          <CardPlanInfo
+            key={index}
+            title={plan.title}
+            icon={plan.icon}
+            description={plan.description}
+            onCheckboxChange={() => handleCheckboxChange(index)}
+            isSelected={selectedPlan === index}
+          />
+        ))}
+      </div>
+
+      {selectedPlan !== null && (
+        <div className="box-products">
+          {products &&
+            map(
+              filter(products, (product) => product.age >= userAge),
+              (product) => {
+                const discount = selectedPlan === 1;
+                return (
+                  <CardProduct
+                    description={product.description}
+                    name={product.name}
+                    discount={discount}
+                    price={product.price}
+                    icon="/products/icon-product-1.svg"
+                  />
+                );
+              }
+            )}
+        </div>
+      )}
+    </MainContainer>
   );
 };
 
